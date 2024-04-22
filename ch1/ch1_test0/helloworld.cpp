a5f576530325ff35b30c12ef364448663791043e
amd_device <wmsjshihao@gmail.com>
2024-04-22 20:02:35 +0800

    1st test

---
+++ ch1/ch1_test0/helloworld.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+#include <tchar.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	MessageBox(NULL,
+		L"Hello World!",
+		L"www.reversecore.com",
+		MB_OK);
+	return 0;
+}
---
+++ reversecore/README.txt
@@ -0,0 +1,4 @@
+《逆向工程核心原理》配书代码
+
+Unzip passowrd:
+reversecore
